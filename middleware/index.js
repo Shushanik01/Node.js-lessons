d9997cc7a21450ab31c74ae453b9635148ed8800
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2026-07-04 13:32:01 +0400

    authenthication middleware & error handling

--- middleware/index.js
+++ middleware/index.js
@@ -1,10 +1,30 @@
 //Authentication Middleware
-function auth(req, res, next){
+function auth(req, res, next) {
 
     const token = req.headers.authorization;
 
-    if(!token){
+    if (!token) {
         res.status(401).send('Unauthorized user')
     }
     next();
+};
+
+// error handling by wrapping controllers in try catch 
+async function getAuthorbyId(req, res) {
+
+    const { authorId } = req.params
+
+    try {
+        const author = await db.find(Number(authorId));
+
+        if (!author) {
+            res.status(404).send('Author not found');
+            return
+        }
+        res.send(`Author: ${author}`)
+
+    } catch (error) {
+        console.log(`Error retriving author`, error);
+        res.status(500).send('Internal server error')
+    }
 }
