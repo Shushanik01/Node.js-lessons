44b303e71a9f3be3d1584c40fd4db9bcad8407df
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2026-07-06 12:29:45 +0400

    feature: validation and sanitization methods created

--- express-validator/storages/usersStorage.js
+++ express-validator/storages/usersStorage.js
@@ -1,33 +1,62 @@
-
+import { body, validationResult, matchedData } from "express-validator";
 
 class UserStorage {
-    constructor(){
+    constructor() {
         this.storage = {}
         this.id = 0
     };
 
-    addUser({firstName, lastName}){
+    addUser({ firstName, lastName }) {
         const id = this.id
-        this.storage[id] = {id, firstName, lastName};
+        this.storage[id] = { id, firstName, lastName };
         this.id++
     };
 
-    getUsers(){
+    getUsers() {
         return Object.values(this.storage)
     };
 
-    getUser(id){
+    getUser(id) {
         return this.storage[id]
     };
 
-    updateUser(id, {firstName, lastName}){
-        this.storage[id] = {id, firstName, lastName}
+    updateUser(id, { firstName, lastName }) {
+        this.storage[id] = { id, firstName, lastName }
     };
 
-    deleteUser(id){
+    deleteUser(id) {
         delete this.storage[id]
     }
 };
- 
+
 let userStorage = new UserStorage
 export default userStorage
+
+const alphaErr = 'must only contain letters';
+const lengthErr = 'should contain 8-12 characters'
+
+const validateUser = [
+    body("firstName").trim()
+        .isAlpha().withMessage(`First name ${alphaErr}`)
+        .isLength({ min: 8, max: 12 }).withMessage(`First name ${lengthErr}`),
+    body("lastName").trim()
+        .isAlpha().withMessage(`Last name ${alphaErr}`)
+        .isLength({ min: 1, max: 10 }).withMessage(`Last name ${lengthErr}`),
+];
+
+export const usersCreatePost = [
+    validateUser,
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).render("createUser", {
+                title: 'Create user',
+                errors: errors.array()
+            });
+        }
+        const { firstName, lastName } = matchedData(req);
+        userStorage.addUser({ firstName, lastName });
+        res.redirect('/')
+
+    }
+]
